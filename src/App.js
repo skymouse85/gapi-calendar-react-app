ca8e6dab4516313a2be74040866ff7b9b6764e9b
Mark Clifford <markmclifford@gmail.com>
2023-02-09 15:44:12 -0800

    initial API setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState } from "react";
+import { gapi } from "gapi-script";
+
 
 function App() {
+  const calendarID = process.env.REACT_APP_CALENDAR_ID;
+  const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+  const accessToken = process.env.REACT_APP_GOOGLE_ACCESS_TOKEN;
+
+  const getEvents = (calendarID, apiKey) => {
+    function initiate() {
+      gapi.client
+        .inti({
+          apiKey: apiKey,
+        })
+
+        .then(function () {
+          return gapi.client.request({
+            path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
+          });
+        })
+
+        .then(
+          (reponse) => {
+            let events = response.result.items;
+            return events;
+          },
+          function (err) {
+            return [false, err];
+          }
+        );
+    }
+
+    gapi.load("client", initiate);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App pt-4" >
+      <h1 className="text-2xl font-bold mb-4">
+        Upcoming Shows and events
+      </h1>
     </div>
   );
 }
+
 
 export default App;
