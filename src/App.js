f0a0d24be416ca862e431da629aab4a1fca26b41
Mark Clifford <markmclifford@gmail.com>
2023-02-11 12:47:04 -0800

    app.js udated to return events

--- src/App.js
+++ src/App.js
@@ -1,47 +1,58 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 import { gapi } from "gapi-script";
-
-
+import Event from "./components/Event.js";
+ 
 function App() {
+  const [events, setEvents] = useState([]);
+ 
   const calendarID = process.env.REACT_APP_CALENDAR_ID;
   const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
   const accessToken = process.env.REACT_APP_GOOGLE_ACCESS_TOKEN;
-
+ 
   const getEvents = (calendarID, apiKey) => {
     function initiate() {
       gapi.client
-        .inti({
+        .init({
           apiKey: apiKey,
         })
-
         .then(function () {
           return gapi.client.request({
             path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
           });
         })
-
         .then(
-          (reponse) => {
+          (response) => {
             let events = response.result.items;
-            return events;
+            setEvents(events);
           },
           function (err) {
             return [false, err];
           }
         );
     }
-
     gapi.load("client", initiate);
-  }
-
+  };
+ 
+  useEffect(() => {
+    const events = getEvents(calendarID, apiKey);
+    setEvents(events);
+  }, [calendarID, apiKey]);
+ 
   return (
-    <div className="App pt-4" >
+    <div className="App py-8 flex flex-col justify-center">
       <h1 className="text-2xl font-bold mb-4">
-        Upcoming Shows and events
+     Upcoming Shows & Events
+        <ul>
+          {events?.map((event) => (
+            <li key={event.id} className="flex justify-center">
+              <Event description={event.summary} />
+            </li>
+          ))}
+        </ul>
       </h1>
     </div>
   );
 }
-
-
+ 
 export default App;
